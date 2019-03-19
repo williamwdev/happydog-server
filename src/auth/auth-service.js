2f8692046889c4f56f20e329ebf395e4ed484100
William Y Wong <wwong1987@gmail.com>
2019-03-19 08:49:31 -0700

    log in feature works, but needs error message for user

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -5,7 +5,7 @@ const config = require('../config');
 
 const AuthService = {
   getUserWithUserName(db, user_name) {
-    return db('blogful_users')
+    return db('happydog_users')
       .where({ user_name })
       .first();
   },
