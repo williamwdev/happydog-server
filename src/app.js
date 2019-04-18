7de8dc4737a670bd0bb75f264104a727df82d2ea
William Y Wong <wwong1987@gmail.com>
2019-04-17 17:25:13 -0700

    debugging 500 error

--- src/app.js
+++ src/app.js
@@ -29,10 +29,10 @@ app.use('/api/folders', foldersRouter);
 // Error handler middleware
 app.use(function errorHandler(error, req, res, next) {
   let response;
+  console.error(error);
   if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } };
   } else {
-    console.error(error);
     response = { message: error.message, error };
   }
   res.status(500).json(response);
